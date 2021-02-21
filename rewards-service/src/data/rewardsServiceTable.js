3f4103a104ccf9a1e4414b29d5b39edaa5a5f04b
Alice Horsey <alicehorsey@gmail.com>
2021-02-21 17:08:59 +0000

    extracts reards table data

---
+++ rewards-service/src/data/rewardsServiceTable.js
@@ -0,0 +1,24 @@
+const rewardsData = {
+    SPORTS: {
+        code: "CHAMPIONS_LEAGUE_FINAL_TICKET",
+        message: "A ticket to the Champion League Final"
+    },
+    KIDS: {
+        code: "N/A",
+        message: ""
+    },
+    MUSIC: {
+        code: "KARAOKE_PRO_MICROPHONE",
+        message: "A Karaoke Pro Microphone"
+    },
+    NEWS: {
+        code: "N/A",
+        message: ""
+    },
+    MOVIES: {
+        code: "PIRATES_OF_THE_CARIBBEAN_COLLECTION",
+        message: "The Pirates of the Caribbean movie collection"
+    }
+};
+
+module.exports = { rewardsData };
