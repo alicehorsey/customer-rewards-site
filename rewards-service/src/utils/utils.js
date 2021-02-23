daf611d23985d715b907cfd2b51845152e6d00b4
Alice Horsey <alicehorsey@gmail.com>
2021-02-23 00:36:33 +0000

    deletes random number function

--- rewards-service/src/utils/utils.js
+++ rewards-service/src/utils/utils.js
@@ -17,9 +17,4 @@ const insertChannelNames = (channelNames) => {
     return response;
 }
 
-//This function generates a random number between 0 and 3 (inclusive) for Eligibilty Spike.
-//There are 4 possible outcome from Eligibilty Service - these will be saved in an array so the random numbers are the array indexes.
-const generateRandomNumber = () => { return Math.floor(Math.random() * 4) };
-console.log(generateRandomNumber())
-
-module.exports = { isAccountNumberValid, insertChannelNames, generateRandomNumber }
+module.exports = { isAccountNumberValid, insertChannelNames }
